343d09a3ee531b96d0c24df3886e32e224286eac
JeyHightower <Jamesehightower@gmail.com>
2025-03-21 19:28:15 -0500

    removed backups

--- frontend-backup-20250320-140850/install-express.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script to install Express for serving the static files
-
-echo "Installing Express for serving static files..."
-npm install --no-save express
-
-echo "Express installation complete."
