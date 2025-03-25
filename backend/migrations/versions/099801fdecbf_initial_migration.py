7bd70019743e9de288d480539f5dd9a6f2c1af9e
JeyHightower <Jamesehightower@gmail.com>
2025-03-25 00:17:46 -0500

    fix: reorder table creation in migration to handle foreign key dependencies

--- backend/migrations/versions/099801fdecbf_initial_migration.py
+++ backend/migrations/versions/099801fdecbf_initial_migration.py
@@ -1,8 +1,8 @@
 """initial migration
 
-Revision ID: 1a86963ef3c9
+Revision ID: 099801fdecbf
 Revises: 
-Create Date: 2025-03-25 00:13:46.982358
+Create Date: 2025-03-25 00:17:42.528734
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1a86963ef3c9'
+revision = '099801fdecbf'
 down_revision = None
 branch_labels = None
 depends_on = None
