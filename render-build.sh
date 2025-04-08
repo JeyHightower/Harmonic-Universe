78f3e0d26831ea19725dc4cf9fd8f7f714b60601
JeyHightower <Jamesehightower@gmail.com>
2025-04-07 23:58:11 -0500

    updated2

--- render-build.sh
+++ render-build.sh
@@ -3,14 +3,33 @@
 # Exit on error
 set -e
 
-# Install frontend dependencies and build
-cd frontend && rm -rf node_modules && npm install && npm run build 
+# Check for frontend directory
+if [ ! -d "frontend" ]; then
+    echo "Frontend directory does not exist."
+    exit 1
+fi
 
-#move to backend
+# Install frontend dependencies and build
+cd frontend
+if [ -d "node_modules" ]; then
+    rm -rf node_modules
+fi
+npm install
+npm run build 
+
+# Check for backend directory
+if [ ! -d "../backend" ]; then
+    echo "Backend directory does not exist."
+    exit 1
+fi
+
+# Move to backend
 cd ../backend
 
 # Install backend dependencies
-pip install -r requirements.txt && pip install psycopg2 
+pip install -r requirements.txt
+pip install psycopg2 
 
 # Run database migrations and seed data
-flask db upgrade && flask seed all 
+flask db upgrade
+flask seed all 
