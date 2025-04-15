8b2f471a9d503245e5811534a19d1e1c2dd5873b
JeyHightower <Jamesehightower@gmail.com>
2025-04-15 00:27:11 -0500

    update

---
+++ frontend/src/components/index.mjs
@@ -0,0 +1,27 @@
+import React from "react";
+
+// Auth Components
+import LoginModal from "../features/auth/modals/LoginModal.jsx";
+import SignupModal from "../features/auth/modals/SignupModal.jsx";
+
+// Navigation Components
+export { default as Navigation } from "./navigation/Navigation.jsx";
+
+// Common Components
+export { Button } from "./common/Button.jsx";
+export { default as Icon } from "./common/Icon.jsx";
+export { default as Input } from "./common/Input.jsx";
+export { default as Select } from "./common/Select.jsx";
+export { default as Slider } from "./common/Slider.jsx";
+export { default as Spinner } from "./common/Spinner.jsx";
+export { default as Tooltip } from "./common/Tooltip.jsx";
+export { default as SafeIcon } from "./common/SafeIcon.jsx";
+
+// Feature Components - Import from feature index files
+export * from "../features/character/index.mjs";
+export * from "../features/note/index.mjs";
+export * from "../features/physics/index.mjs";
+export * from "../features/universe/index.mjs";
+
+// Modal Components
+export * from "./modals/index.mjs";
