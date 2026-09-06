ed91159b4f85157519dcc0aab57f94ef877192fe
JeyHightower <Jamesehightower@gmail.com>
2026-09-06 18:12:38 -0500

    ready for production

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,7 @@ from flask_cors import CORS
 from config import Config, db, jwt
 from models import TokenBlocklist
 from seed import demo_seed_data
-# from models import User, Universe, character_universes, AlignmentType, Character, TokenBlocklist, Location, Note, LocationType, character_notes, note_universes, character_locations, location_notes
 from routes import auth_bp, universe_bp, character_bp, note_bp, location_bp, user_bp
-# from utils import get_current_user, get_owned_universe_ids, get_request_universe_ids, character_autherization, check_if_token_revoked
 
 
 app = Flask(__name__)
@@ -14,10 +12,7 @@ CORS(app)
 db.init_app(app)
 jwt.init_app(app)
 
-#! Link to frontend
-# @app.route('/api/test-connection')
-# def test_connection():
-#     return {"status": "Success!!!"}
+
 
 all_blueprints = [
     (auth_bp, '/api/auth'),
